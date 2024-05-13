023a95b5764d2512dd7055d010b46f737975ad85
Isaia Riva <isaia.riva@finconsgroup.com>
2024-05-13 09:27:37 +0200

    init develop smart-html-select-plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,98 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, WorkspaceWindow } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class SelectOnFirePlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		this.app.workspace.on("window-open", this.setupWindowHandlers);
+		this.setupWindowHandlers(undefined as never, activeWindow);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		this.addRibbonIcon("flame", "Insert a HTML select", () => {
+			const view = this.app.workspace.activeEditor;
+			if (!view?.editor) {
+				return;
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+			const randomId = this.generateUniqueId(
+				view.editor.getDoc().getValue()
+			);
+			const select = createEl("select", { attr: { id: randomId } });
+			for (let i = 1; i <= 5; i++) {
+				const option = createEl("option", {
+					text: `option ${i}`,
+					attr: { value: `${i}` },
+				});
+				select.appendChild(option);
 			}
+			view.editor.replaceSelection(select.outerHTML);
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+	}
+	async onunload() {
+		this.app.workspace.off("window-open", this.setupWindowHandlers);
+	}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+	private generateUniqueId(page: string): string {
+		let id = crypto.randomUUID().slice(-6);
+		while (this.idExistsInFile(id, page)) {
+			id = crypto.randomUUID();
+		}
+		return id;
+	}
+
+	private idExistsInFile(id: string, page: string): boolean {
+		const idIndex = page.search(id);
+		return idIndex !== -1;
+	}
+
+	private setupWindowHandlers = (
+		_workspaceWindow: WorkspaceWindow,
+		win: Window
+	) => {
+		this.registerDomEvent(
+			win,
+			"change",
+			async (evt: Event): Promise<void> => {
+				const changeEl = evt.target as HTMLSelectElement;
+
+				if (!changeEl?.id && !(changeEl instanceof HTMLSelectElement)) {
+					return;
 				}
+
+				const selectedOption =
+					changeEl.options[changeEl.selectedIndex].value;
+				const selectId = changeEl.id;
+				const NumberOfOptions = changeEl.options.length;
+
+				const view = this.app.workspace.activeEditor;
+				if (!view || !view.editor || !view.file) {
+					return;
+				}
+
+				const options = changeEl.options;
+
+				Array.from(options).map(({ value, text }) => {
+					const option = createEl("option", {
+						text,
+						attr: {
+							value,
+							...(value === selectedOption && {
+								selected: "selected",
+							}),
+						},
+					});
+					changeEl.appendChild(option);
+				});
+
+				for (let i = 1; i <= NumberOfOptions; i++) {
+					changeEl.remove(0);
+				}
+
+				let page = await this.app.vault.read(view.file);
+
+				page = page.replace(
+					new RegExp(`<select id="${selectId}">[\\s\\S]*?</select>`),
+					`${changeEl.outerHTML}`
+				);
+
+				this.app.vault.modify(view.file, page);
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+		);
+	};
 }
